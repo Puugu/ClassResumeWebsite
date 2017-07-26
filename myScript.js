4a0c6db5fc801e7f25f375e067786dc08e80dbb8
Puugu <therealckirkham@gmail.com>
2017-07-26 17:34:54 -0500

    add file for jQuery, edit index.html, and hide contact form on pageload

---
+++ myScript.js
@@ -0,0 +1,11 @@
+/* jQuery File for CSS 405 Web Dev Course
+USM Summer 2017
+Created by Courtney Kirkham
+Creation Date: 26 July 2017 */
+
+$(document).ready(function() {
+	
+	//Function to hide webform on pageload
+	$("#formSection").hide();
+	
+});
